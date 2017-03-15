4b544dbc5453ee7761f6c0436e45d4419a63850f
BM-44071 <k.smruthi.smruthi@gmail.com>
2017-03-15 12:28:22 -0700

    sprint1 4 pages designs

---
+++ fb2.php
@@ -0,0 +1,66 @@
+<html lang="en">
+<head>
+<title></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-sm-2">
+</div>
+ 
+    <div class="col-sm-8">
+     <!-- <div class="jumbotron text-center"> -->
+      <div id="fb1" style="background-color: darkblue;margin-top:-20px; height:1.5cm;"> 
+           <div id="fb2" style="color:white;margin-top: 1px;"><h1><center>facebook</center></h1> </div> 
+    
+   <div class="col-sm-2">
+      
+    </div>
+
+  </div>
+</div></div>
+ <div class="row"> 
+  <div class="col-sm-2"></div> 
+  <div class="col-sm-8"> 
+   <div id="fb3" style="background-color: #edf0f5; height:12cm; ">
+     <pre style="height:15.7cm;">
+      <hr style="height:1px;border:none;color:#333;background-color:#333;margin-top: 11.7cm;"> 
+     </pre>
+   </div>
+
+  
+   
+
+     <input type="text" placeholder="Email address or phone number" size="40" id="fb4" style="margin-left:6.3cm; margin-top:-365px;height: 1cm; border-radius: 6px 6px 0px 0px;"> 
+     <input type="password" placeholder="Password" size="40" style="margin-left: 6.3cm; margin-top: -342px; height:1cm; border-radius:0px 0px 6px 6px;"> 
+     <input type="button" value="Log In" style="margin-top:-288px; width:7.3cm;margin-left:-7.3cm; height:1cm; background-color:darkblue; color:white; border-radius: 6px;"> 
+
+ <input type="submit" value="Create New Account" style="margin-left: 7.9cm;margin-top: -220px; background-color: blue; height: 1cm; border-radius: 6px;color:white;">
+<p id="fb4" style="margin-left: 7cm;margin-top: -150px;color:darkblue;">Forgotten password? .Help centre</p> 
+ </div>
+
+<div class="col-sm-2"></div> 
+</div>
+<div class="row"> 
+  <div class="col-sm-2"></div> 
+  <div class="col-sm-8"> 
+
+<div id="fb5" style=" color:blue;margin-top: 120px; margin-left: 3cm;"><h5>ENGLISH</h5></div>
+<div id="fb6" style="margin-left: 3cm; color:blue;">HINDI</div>
+<div id="fb7" style=" color:blue;margin-left: 3cm; margin-top: 10px;">TAMIL</div>
+<div id="fb8" style=" color:blue;margin-left: 3cm;margin-top:10px; ">URUDU</div>
+<div id="fb9" style=" color:blue;margin-left: 13cm;margin-top: -110px;">MALAYALAM</div>
+<div id="fb10" style="color:blue;margin-left: 13cm;margin-top: 8px;">KANNADA</div>
+<div id="fb11" style="color:blue; margin-left: 13cm;margin-top: 5px;">ODIA</div>
+<div id="fb12" style="color:blue; margin-left: 13cm;margin-top: 5px;">MORE LANGUAGES...</div>
+
+</div>
+<div class="col-sm-2"></div> 
+</div>
+</body>
+</html>
---
+++ fb4.php
@@ -0,0 +1,61 @@
+<html lang="en">
+
+<head>
+<link rel="stylesheet"  type="text/css" href="outline.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+ <div class="container-fluid">
+<div class="row">
+<div class="col-md-12" style="height:2.5cm;background-color:darkblue;">
+<h2 id="fb2" style="margin-left: 5cm; color:white;margin-top: 26px;">facebook</h2>
+<input type="submit" value="Sign Up" style="margin-left:8.5cm;margin-top:-43px;color:white;background-color: green;">
+
+</div>
+ 
+</div>
+<div class="row">
+<div class="col-md-12" style="background-color: #edf0f5;height:90px;"></div></div>
+<div class="row">
+<div class="col-md-3" style="background-color: #edf0f5;height:300px;"></div>
+<div class="col-md-6" style="background-color: white; height:300px; box-shadow: 1px 1px 1px 1px #888888;">
+ <h4>Facebook Login</h4>
+ <hr style="margin-top: -2px;">
+ <h5 style="margin-left: 2.5cm;margin-top: 35px;"><b>Email or Phone:</b></h5>
+ <input type="text" size="40" style="margin-left:6cm ;margin-top: -40px;">
+ <h5 style="margin-left: 2.5cm;margin-top: 22px;"><b>Password:</b></h5>
+ <input type="text" size="40" style="margin-left: 6cm;margin-top: -40px;">
+ <input type="checkbox" style="margin-top:5px;margin-left: 6.1cm; ">Keep me logged in.
+ <br>
+
+ <input type="submit" value="Log In" size="4" style="background-color: darkblue;color:white;margin-left: 6cm;margin-top: 30px; ">
+ <h5 style="margin-left: 7.6cm;margin-top: -20px;">or <b style="color: darkblue;">Sign Up for Facebook.</b></h5>
+ <h6 style="color: blue;margin-left: 6.1cm;margin-top: 30px;">Forgotten password?</h6>
+</div>
+<div class="col-md-3" style="background-color: #edf0f5;height:300px;"></div>
+</div>
+
+<div class="row">
+<div class="col-md-12" style="background-color: #edf0f5;height:90px;"></div></div>
+<div class="row">
+<div class="col-md-12" style="background-color: white;height: 90px;">
+<h5 style="margin-left: 4cm;color:blue;">ENGLISH</h5> 
+ <h5 style="margin-left: 6.5cm;margin-top:-25PX;color:blue; ">MALAYALAM</h5>
+ <h5 style="margin-left: 9.7cm;margin-top: -25px;color:blue;">HINDI</h5>
+ <h5 style="margin-left: 11.5cm;margin-top: -25px;color:blue;">TAMIL</h5>
+ <h5 style="margin-left: 13cm;margin-top: -25px;color:blue;">URUDU</h5>
+ <h5 style="margin-left: 15cm;margin-top: -25px;color:blue;">PORTUGESE</h5>
+ <hr style="margin-left: 3cm;margin-top: -7px;">
+ <h5 style="margin-left: 4cm;margin-top: -20px;color:blue;">Sign up</h5>
+ <h5 style="margin-left:6.5cm;margin-top: -24px;color:blue; ">Log in</h5>
+ <h5 style="margin-left: 9cm;margin-top: -23px;color:blue;">Messenger</h5>
+ <h5 style="margin-left: 11.7cm;margin-top: -24px;color:blue;">Facebook Lite</h5>
+</div></div>
+</div>
+</body>
+
+</html>
