e045f45c70ba86765b11aa1ebe85f00fe8fd43cb
BM-44071 <k.smruthi.smruthi@gmail.com>
2017-03-28 11:25:03 -0700

    sprint1 4 pages designs

---
+++ fb5.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <?php $this->load->helper('url');?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+    <div class="row">
+<div class="col-md-12" style="height:2.5cm;background-color:darkblue;">
+ <input type="text" size="50" style="height:0.8cm;margin-left:2.5cm;margin-top: 30px;">
+        <button type="button" class="btn btn-default btn-sm   glyphicon glyphicon-search" style="margin-left:-5px;height:0.8cm;margin-top:-4px;background-color: #edf0f5;">
+           Search 
+        </button>
+  <img src="<?php echo base_url().'images/sidhu.jpg';?>" style="height:1.7cm;margin-left: 10cm;margin-top: -1px;width: 1.5cm;">
+  <h5 style="margin-left: 25cm;margin-top: -39px;color:white;">Smruthi.k</h5>
+</div>
+</div>
+<div class="row">
+<div class="col-md-2" style="height: 18cm;background-color: #edf0f5;">
+<img src="<?php echo base_url().'images/sidhu.jpg';?>" style="height:0.8cm;margin-top: 25px;width:30px;">
+<h6 style="size: 20;margin-left: 1.2cm;margin-top: -20px;">Smruthi.k</h6>
+<img src="<?php echo base_url().'images/edit.jpg';?>" style="height: 0.8cm;margin-top: 13px;">
+<h6 style="size:20;margin-left: 1.2cm;margin-top:-16px; ">Edit profile</h6>
+</div>
+<div class="col-md-7" style="height: 18cm;background-color: #edf0f5;">
+<div style="height: 5.1cm;margin-top:0.4cm;  "><pre style="height: 5.1cm;background-color: white;">
+<img src="<?php echo base_url().'images/edit.jpg';?>" style="height: 0.8cm;">
+<h6 style="margin-left:1.1cm; margin-top: -20px;"><b>Update status</b></h6>
+<img src="<?php echo base_url().'images/pho.jpg';?>" style="height: 0.8cm; margin-left:5.4cm;margin-top: -83px; ">
+<h6 style="margin-left:6.8cm; margin-top: -60px;color: blue;"><b>Add Photos/Video</b></h6>
+<img src="<?php echo base_url().'images/note.jpg';?>" style="height: 0.8cm;margin-left: 12cm;margin-top:-83px; ">
+<h6 style="margin-left:13.6cm; margin-top: -60px;color: blue;"><b>Write Note</b></h6>
+<hr style="margin-top: -20px;">
+ <img src="<?php echo base_url().'images/sidhu.jpg';?>" style="height:1.4cm;margin-left: 1cm;margin-top: -1px;width: 1.1cm;">
+ <textarea rows="4" cols="50" placeholder="What's on your mind?" style="border:0px solid grey; margin-left: 2.6cm;margin-top: -40px;"></textarea>
+</pre>
+</div>
+<div style="background-color:white;height:9cm;margin-top: 17px;">
+<pre style="height: 9cm; background-color: white;">
+	
+</pre>
+</div>
+</div>
+<div class="col-md-3" style="height: 18cm;background-color: #edf0f5;">
+<div style="background-color:white;height: 15.2cm;margin-left:-15px; ">
+<pre style="height: 15.2cm;background-color: white;margin-top: 16px;">
+<h6><b>ADD STATUS</b></h6>
+<div style="height: 13cm;background-color: blue;margin-top: -0.2cm;">
+</pre>
+</div>
+</div>
+</div>
+</body></html>
+      
